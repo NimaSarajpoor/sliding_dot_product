5834daf6d553811524d49d5d976703e4dc61dd14
NimaSarajpoor <nimasarajpoor@gmail.com>
2025-03-09 17:15:46 -0400

    minor change

--- python_version.sh
+++ python_version.sh
@@ -11,14 +11,13 @@ check_errs()
 }
 
 get_safe_python_version()
-{   
-    rm -rf tmp.txt
-    curl -s --location https://devguide.python.org/versions -o tmp.txt
-    
-    SAFE_PYTHON=$(sed 's/<svg[^>]*>//g' tmp.txt | xmllint --html --xpath '//section[@id="supported-versions"]//table/tbody/tr[count(//section[@id="supported-versions"]//table/tbody/tr[td[.="security"]]/preceding-sibling::*)]/td[1]/p/text()' - 2> /dev/null)
-    # SAFE_PYTHON=$(curl -s --location https://devguide.python.org/versions | xmllint --html --xpath '//section[@id="supported-versions"]//table/tbody/tr[count(//section[@id="supported-versions"]//table/tbody/tr[td[.="security"]]/preceding-sibling::*)]/td[1]/p/text()' - 2> /dev/null)
-    check_errs $?
-    rm -rf tmp.txt
+{
+  rm -rf tmp.txt
+  curl -s --location https://devguide.python.org/versions -o tmp.txt
+  SAFE_PYTHON=$(cat tmp.txt | xmllint --html --xpath '//section[@id="supported-versions"]//table/tbody/tr[count(//section[@id="supported-versions"]//table/tbody/tr[td[.="security"]]/preceding-sibling::*)]/td[1]/p/text()' - 2> /dev/null)
+  # SAFE_PYTHON=$(curl -s --location https://devguide.python.org/versions | xmllint --html --xpath '//section[@id="supported-versions"]//table/tbody/tr[count(//section[@id="supported-versions"]//table/tbody/tr[td[.="security"]]/preceding-sibling::*)]/td[1]/p/text()' - 2> /dev/null)
+  check_errs $?
+  rm -rf tmp.txt
 }
 
 get_safe_python_version
